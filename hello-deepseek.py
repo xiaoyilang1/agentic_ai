7342b0aff729530657a9b200228f1861c96cd981
xiaoyilang1 <3240513087@qq.com>
2026-05-31 22:29:07 +0800

    Create a new agentic item

---
+++ hello-deepseek.py
@@ -0,0 +1,23 @@
+# Please install OpenAI SDK first: `pip3 install openai`
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
+client = OpenAI(
+    api_key=os.environ.get('DEEPSEEK_API_KEY'),
+    base_url="https://api.deepseek.com")
+
+response = client.chat.completions.create(
+    model="deepseek-v4-flash",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant"},
+        {"role": "user", "content": "Hello"},
+    ],
+    stream=False,
+    reasoning_effort="high",
+    extra_body={"thinking": {"type": "enabled"}}
+)
+
+print(response.choices[0].message.content)
